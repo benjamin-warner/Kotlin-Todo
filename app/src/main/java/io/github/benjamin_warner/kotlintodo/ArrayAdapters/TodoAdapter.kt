9761e2480324b5fc59a012974bfa09770d910152
Ben Warner <benjaminnwarner@gmail.com>
2018-03-27 20:15:19 -0400

    Todos list on screen

---
+++ app/src/main/java/io/github/benjamin_warner/kotlintodo/ArrayAdapters/TodoAdapter.kt
@@ -0,0 +1,43 @@
+package io.github.benjamin_warner.kotlintodo.ArrayAdapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import io.github.benjamin_warner.kotlintodo.R
+
+class TodoAdapter(context: Context?, resource: Int, todos: ArrayList<String>) : ArrayAdapter<String>(context, resource, todos) {
+
+    private var todos: ArrayList<String> = todos
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+
+        val view: View?
+        val item = getItem(position)
+
+        if(convertView == null){
+            val layoutInflater = LayoutInflater.from(context)
+            view = layoutInflater.inflate(R.layout.todo_list_item, parent, false)
+
+            val todoText = view.findViewById(R.id.todo_text) as TextView
+            todoText.text = item
+
+            view.findViewById(R.id.remove_todo).setOnClickListener{ removeToDo(position) }
+        }
+        else{
+            view = convertView
+        }
+
+        return view
+    }
+
+    private fun removeToDo(position: Int) {
+        todos.removeAt(position)
+    }
+
+    fun addToDo(text: String){
+        todos.add(text)
+    }
+}
--- app/src/main/java/io/github/benjamin_warner/kotlintodo/MainActivity.kt
+++ app/src/main/java/io/github/benjamin_warner/kotlintodo/MainActivity.kt
@@ -2,11 +2,31 @@ package io.github.benjamin_warner.kotlintodo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.ListView
+import io.github.benjamin_warner.kotlintodo.ArrayAdapters.TodoAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var newTodoEditText: EditText
+    private lateinit var todoAdapter: TodoAdapter
+    private lateinit var todoListView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        newTodoEditText = findViewById(R.id.todo_text) as EditText
+
+        todoAdapter = TodoAdapter(this, R.layout.todo_list_item, ArrayList())
+        todoListView = findViewById(R.id.todo_list) as ListView
+        todoListView.adapter = todoAdapter
+
+        findViewById(R.id.add_todo).setOnClickListener { addNewToDo() }
+    }
+
+    private fun addNewToDo() {
+        todoAdapter.addToDo(newTodoEditText.text.toString())
+        todoAdapter.notifyDataSetChanged()
     }
 }
